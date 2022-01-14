7740f9b44fa81da41392064e920c48b23a565413
Thiago Coutinho <tcoutinho@navisite.com>
2022-01-14 21:54:14 +0100

    adding token to gitignore

--- renovate/renovate.sh
+++ renovate/renovate.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+source token.env
 
 docker run -v /var/run/docker.sock:/var/run/docker.sock \
            -v "$(pwd)/config.js:/usr/src/app/config.js" \
            --rm --privileged -it --user root \
-           renovate/renovate:latest renovate --dry-run --token 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 $(cat ./repositories.txt)
+           renovate/renovate:latest renovate --dry-run --token $TOKEN $(cat ./repositories.txt)
